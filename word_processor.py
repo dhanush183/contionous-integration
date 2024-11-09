7550ecdffaf58eb7f683406c77326a3d7f5e4847
dhanush183 <dhanush.janapareddy2002@gmail.com>
2024-11-09 21:23:44 +0100

    now i will commit and push the function snd test case to check whetger its passes or not

--- word_processor.py
+++ word_processor.py
@@ -1,3 +1,4 @@
 class WordProcessor:
+    """A class to process text strings with various methods"""
     def count_alphabets(self, text):
         return sum(1 for char in text if char.isalpha())
--- word_test.py
+++ word_test.py
@@ -2,7 +2,7 @@ from word_processor import WordProcessor
 word_processor = WordProcessor()
 
 def test_count_alphabets():
-    text = "Hello World 123"
+    text = "Hello World 123" #input text for count_alphabets
     exp_result = 10
     result = word_processor.count_alphabets(text)
     assert result == exp_result, f"Expected: {exp_result}, but got: {result}"
